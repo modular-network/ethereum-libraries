9491ce510dffb0e2844a749ab35e32a5b5176426
Hackdom <hackdom@users.noreply.github.com>
2017-07-23 14:08:52 -0500

    More fixes

--- ERC20Lib/truffle/truffle.js
+++ ERC20Lib/truffle/truffle.js
@@ -1,9 +1,3 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-
-var mnemonic = "refuse blade over muscle crime flat merge fluid comfort faint recycle lobster";
-//var provider = new HDWalletProvider(mnemonic, "https://ropsten.infura.io/");
-//console.log(provider.getAddress());
-
 module.exports = {
   networks: {
     development: {
