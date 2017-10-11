3b99c937384401d7cf0aa84b36bdd75f84086da0
Paulo Pires <taralloqueiroiz@gmail.com>
2017-10-11 14:13:04 -0300

    chore(tests): finish refactor of +2 tests cases

--- WalletLib/truffle/test/walletLib.js
+++ WalletLib/truffle/test/walletLib.js
@@ -86,7 +86,6 @@ contract('WalletLibTestContract', (accounts) => {
                          true, {from: accounts[0]});
     const ret = await c.addOwner("0x36994c7cff11859ba8b9715120a68aa9499329ee",
                          true, {from: accounts[0]});
-    console.log(ret.logs[0].args);
     const id = ""+ret.logs[0].args.txid+"";
     await c.revokeConfirm(id, {from:accounts[0]});
     const len = await c.transactionLength(id);
@@ -128,7 +127,6 @@ contract('WalletLibTestContract', (accounts) => {
     const ret2 = await c.removeOwner("0x0deef860f84a5298ccbc8a56f32f6ce49a236c8e",
                          true, {from: accounts[0]});
     
-    console.log(ret2.logs[0].args);
     const id2 = ""+ret2.logs[0].args.txid+"";
     await c.removeOwner("0x0deef860f84a5298ccbc8a56f32f6ce49a236c8e",
                          false, {from: accounts[2]});
@@ -308,110 +306,65 @@ contract('WalletLibTestContract', (accounts) => {
     assert.equal(count2.valueOf(), 1, "Confirmation count should be one b/c accounts[0] has already confirmed");
     assert.equal(b.valueOf(), 8, "2 tokens should be transferred to accounts[5] after 2 sigs");
   });
-  it("should execute major tx after requiredMajor number of confirmations and deny illegal requests", function() {
-    let c;
-    let tc;
-    let tcAdd;
+  it("should execute major tx after requiredMajor number of confirmations and deny illegal requests", async () => {
     let id;
-    let initialBalance;
-    let data;
 
-    return WalletLibTestContract.deployed().then(function(instance){
-      c = instance;
-      return TestToken.deployed().then(function(tokInstance){
-        tc = tokInstance;
-        tcAdd = ""+tc.address+"";
-        return web3.eth.getBalance(accounts[5]);
-      }).then(function(bal){
-        initialBalance = Math.floor(bal.valueOf()/10**18);
-        return c.sendTransaction({value: 100000000000000000000, from: accounts[5]});
-      }).then(function(ret){
-        return web3.eth.getBalance(accounts[5]);
-      }).then(function(bal){
-        bal = initialBalance - Math.floor(bal.valueOf()/10**18);
-        assert.equal(bal, 100, "100 ether should be transferred to the wallet from accounts[5]");
-        return c.serveTx(accounts[5], 60000000000000000000, 0, true, {from: accounts[0]});
-      }).then(function(ret){
-        console.log(ret.logs[0].args);
-        id = ""+ret.logs[0].args.txid+"";
-        return c.confirmTx(id, {from:accounts[2]});
-      }).then(function(){
-        return web3.eth.getBalance(accounts[5]);
-      }).then(function(bal){
-        bal= initialBalance - Math.floor(bal.valueOf()/10**18);
-        assert.equal(bal, 100, "No ether should be sent until 4 confirms");
-        return c.confirmTx(id, {from:accounts[3]});
-      }).then(function(bal){
-        return c.confirmTx(id, {from:accounts[1]});
-      }).then(function(ret){
-        console.log(ret.logs[0].args);
-        return web3.eth.getBalance(accounts[5]);
-      }).then(function(bal){
-        bal = initialBalance - Math.floor(bal.valueOf()/10**18);
-        assert.equal(bal, 40, "60 ether should be transferred to accounts[5] from the wallet with 4 sigs");
-      }).then(function(){
-        return tc.transfer.request(accounts[5], 5);
-      }).then(function(ret){
-        data = ret.params[0].data;
-        return c.serveTx(tcAdd, 0, ""+data+"", true, {from: accounts[0]});
-      }).then(function(ret){
-        console.log(ret.logs[0].args);
-        id = ""+ret.logs[0].args.txid+"";
-        return c.revokeConfirm(id, {from:accounts[2]});
-      }).then(function(ret){
-        return c.transactionLength(id);
-      }).then(function(len){
-        len = len.valueOf();
-        return c.transactionConfirmCount(id, len - 1);
-      }).then(function(count){
-        count = count.valueOf();
-        assert.equal(count, 1, "Confirmation count should be one b/c accounts[2] has not confirmed");
-      }).then(function(){
-        return c.serveTx(tcAdd, 0, ""+data+"", true, {from: accounts[0]});
-      }).then(function(ret){
-        return c.transactionLength(id);
-      }).then(function(len){
-        len = len.valueOf();
-        return c.transactionConfirmCount(id, len - 1);
-      }).then(function(count){
-        count = count.valueOf();
-        assert.equal(count, 1, "Confirmation count should be one b/c accounts[0] has already confirmed");
-      }).then(function(){
-        return c.confirmTx(id, {from:accounts[2]});
-      }).then(function(){
-        return c.confirmTx(id, {from:accounts[3]});
-      }).then(function(){
-        return c.confirmTx(id, {from:accounts[1]});
-      }).then(function(ret){
-        console.log(ret.logs[0].args);
-        return tc.balanceOf(""+c.address+"");
-      }).then(function(b){
-        assert.equal(b.valueOf(), 3, "3 tokens should be transferred to accounts[5] after 4 sigs");
-      });
-    });
+    const c = await WalletLibTestContract.deployed();
+    const tc = await TestToken.deployed();
+
+    const tcAdd = ""+tc.address+"";
+    const bal = await web3.eth.getBalance(accounts[5]);
+    const initialBalance = Math.floor(bal.valueOf()/10**18);
+    await c.sendTransaction({value: 100000000000000000000, from: accounts[5]});
+    const bal2 = await web3.eth.getBalance(accounts[5]);
+    const realBalance = initialBalance - Math.floor(bal2.valueOf()/10**18);
+    
+    const ret = await c.serveTx(accounts[5], 60000000000000000000, 0, true, {from: accounts[0]});
+    id = ""+ret.logs[0].args.txid+"";
+    await c.confirmTx(id, {from:accounts[2]});
+    const bal3 = await web3.eth.getBalance(accounts[5]);
+    const realBalance2 = initialBalance - Math.floor(bal3.valueOf()/10**18);
+    
+    await c.confirmTx(id, {from:accounts[3]});
+    await c.confirmTx(id, {from:accounts[1]});
+    const bal4 = await web3.eth.getBalance(accounts[5]);
+    const realBalance3 = initialBalance - Math.floor(bal4.valueOf()/10**18);
+    
+    const ret2 = await tc.transfer.request(accounts[5], 5);
+    const data = ret2.params[0].data;
+    const ret3 = await c.serveTx(tcAdd, 0, ""+data+"", true, {from: accounts[0]});
+    id = ""+ret3.logs[0].args.txid+"";
+    await c.revokeConfirm(id, {from:accounts[2]});
+    const len = await c.transactionLength(id);
+    const count = await c.transactionConfirmCount(id, len.valueOf() - 1);
+    
+    await c.serveTx(tcAdd, 0, ""+data+"", true, {from: accounts[0]});
+    const len2 = await c.transactionLength(id);
+    const count2 = await c.transactionConfirmCount(id, len2.valueOf() - 1);
+    
+    await c.confirmTx(id, {from:accounts[2]});
+    await c.confirmTx(id, {from:accounts[3]});
+    await c.confirmTx(id, {from:accounts[1]});
+    const b = await tc.balanceOf(""+c.address+"");
+
+    assert.equal(realBalance, 100, "100 ether should be transferred to the wallet from accounts[5]");
+    assert.equal(realBalance2, 100, "No ether should be sent until 4 confirms");
+    assert.equal(realBalance3, 40, "60 ether should be transferred to accounts[5] from the wallet with 4 sigs");
+    assert.equal(count.valueOf(), 1, "Confirmation count should be one b/c accounts[2] has not confirmed");
+    assert.equal(count2.valueOf(), 1, "Confirmation count should be one b/c accounts[0] has already confirmed");
+    assert.equal(b.valueOf(), 3, "3 tokens should be transferred to accounts[5] after 4 sigs");
+    
   });
-  it("should create contract after appropriate number of sigs, no target, and proper data", function() {
-    let c;
-    let id;
+  it("should create contract after appropriate number of sigs, no target, and proper data", async () => {
     let data  = "0x6060604052341561000f57600080fd5b5b6103108061001f6000396000f300606060405263ffffffff60e060020a6000350416631d3b9edf811461004557806366098d4f1461006d578063e39bbf6814610095578063f4f3bdc1146100bd575b600080fd5b6100536004356024356100e5565b604051911515825260208201526040908101905180910390f35b610053600435602435610159565b604051911515825260208201526040908101905180910390f35b6100536004356024356101cd565b604051911515825260208201526040908101905180910390f35b610053600435602435610247565b604051911515825260208201526040908101905180910390f35b600082820282158382048514176100fe57506001905060005b8115610151576000805160206102c58339815191526040516020808252601390820152606860020a7274696d65732066756e63206f766572666c6f77026040808301919091526060909101905180910390a15b5b9250929050565b600082820182810384148382111661017357506001905060005b8115610151576000805160206102c58339815191526040516020808252601290820152607060020a71706c75732066756e63206f766572666c6f77026040808301919091526060909101905180910390a15b5b9250929050565b60008082156101e85750818304806020604051015260408051f35b6000805160206102c58339815191526040516020808252601790820152604860020a76747269656420746f20646976696465206279207a65726f026040808301919091526060909101905180910390a1506001905060005b9250929050565b60008183038083018414848210828614171660011461026857506001905060005b8115610151576000805160206102c58339815191526040516020808252601490820152606060020a736d696e75732066756e6320756e646572666c6f77026040808301919091526060909101905180910390a15b5b925092905056004eb9487277c052fc38bc53c91e4af51b26a1e7600aa1761ef9d2973180cf72a7a165627a7a72305820816921a222288d2b0efcc39a7dfdda6552de32e026d6a7f8b9e335dab1871dcb0029";
 
-    return WalletLibTestContract.deployed().then(function(instance){
-      c = instance;
-
-      return c.serveTx(0, 0, data, true, {from: accounts[0]});
-    }).then(function(ret){
-      console.log(ret.logs[0].args);
-      id = ""+ret.logs[0].args.txid+"";
-      return c.confirmTx(id, {from:accounts[2]});
-    }).then(function(ret){
-      console.log(ret.logs[0].args);
-      return c.confirmTx(id, {from:accounts[1]});
-    }).then(function(ret){
-      console.log(ret.logs[0].args);
-      return c.confirmTx(id, {from:accounts[3]});
-    }).then(function(ret){
-      console.log(ret.logs[0].args);
-      assert.isDefined(ret.logs[0].args.newContract, "New contract should be created if no target and proper data");
-    });
+    const c = await WalletLibTestContract.deployed();
+    const ret = await c.serveTx(0, 0, data, true, {from: accounts[0]});
+    const id = ""+ret.logs[0].args.txid+"";
+    await c.confirmTx(id, {from:accounts[2]});
+    await c.confirmTx(id, {from:accounts[1]});
+    const ret2 = await c.confirmTx(id, {from:accounts[3]});
+    
+    assert.isDefined(ret2.logs[0].args.newContract, "New contract should be created if no target and proper data");
   });
 });
