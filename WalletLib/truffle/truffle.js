e2782266aa58033cd3884ae7c30d191ad0d89423
Paulo Pires <taralloqueiroiz@gmail.com>
2017-10-17 20:04:22 -0200

    chore(build): testing travis.ci config

--- WalletLib/truffle/truffle.js
+++ WalletLib/truffle/truffle.js
@@ -6,13 +6,6 @@ module.exports = {
       gas: 270000000000000,
       from: "0x40333d950b4c682e8aad143c216af52877d828bf",
       network_id: "*"
-    },
-    coverage: {
-      host: "localhost",
-      port: 8544,
-      gas: 270000000000000,
-      from: "0x40333d950b4c682e8aad143c216af52877d828bf",
-      network_id: "*"
     }
   }
 };
