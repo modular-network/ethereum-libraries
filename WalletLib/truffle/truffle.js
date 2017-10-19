ba4c4f7d9b0590e675a60fe2665a2a53a9a3926f
Paulo Pires <taralloqueiroiz@gmail.com>
2017-10-19 17:28:19 -0200

    chore(tests): fixing coverage

--- WalletLib/truffle/truffle.js
+++ WalletLib/truffle/truffle.js
@@ -1,5 +1,14 @@
 module.exports = {
   networks: {
+    coverage: {
+      host: "localhost",
+      port: 8555,
+      gas: 270000000000000,
+      from: "0x40333d950b4c682e8aad143c216af52877d828bf",
+      network_id: "*",
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
     development: {
       host: "localhost",
       port: 8545,
