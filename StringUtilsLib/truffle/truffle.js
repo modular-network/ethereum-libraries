2a542544cf9a7805734c45fade2ec959b26753dd
Paulo Pires <taralloqueiroiz@gmail.com>
2017-10-25 14:16:26 -0200

    chore(coverage): cleaning up config files

--- StringUtilsLib/truffle/truffle.js
+++ StringUtilsLib/truffle/truffle.js
@@ -9,8 +9,6 @@ module.exports = {
     coverage: {
       host: "localhost",
       port: 8555,
-      gas: 270000000000000,
-      from: "0x40333d950b4c682e8aad143c216af52877d828bf",
       network_id: "*",
       gas: 0xfffffffffff,
       gasPrice: 0x01
