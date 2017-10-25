fd625983635ae860b6e8e873e19c63ed319394a8
Paulo Pires <taralloqueiroiz@gmail.com>
2017-10-25 11:02:41 -0200

    chore(coverage): DirectCrowdsaleLib coverage setup fix

--- CrowdsaleLib/DirectCrowdsale/truffle/truffle.js
+++ CrowdsaleLib/DirectCrowdsale/truffle/truffle.js
@@ -8,7 +8,6 @@ module.exports = {
     coverage: {
       host: "localhost",
       port: 8555,
-      from: "0x40333d950b4c682e8aad143c216af52877d828bf",
       network_id: "*",
       gas: 0xfffffffffff,
       gasPrice: 0x01
