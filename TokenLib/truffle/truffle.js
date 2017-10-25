5db28d088dca1225a7c553706ee29266bd92dfc8
Paulo Pires <taralloqueiroiz@gmail.com>
2017-10-24 23:57:45 -0200

    chore(coverage): TokenLib coverage setup

--- TokenLib/truffle/truffle.js
+++ TokenLib/truffle/truffle.js
@@ -4,6 +4,15 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*"
-    }
+    },
+    coverage: {
+      host: "localhost",
+      port: 8555,
+      gas: 270000000000000,
+      from: "0x40333d950b4c682e8aad143c216af52877d828bf",
+      network_id: "*",
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
   }
 };
