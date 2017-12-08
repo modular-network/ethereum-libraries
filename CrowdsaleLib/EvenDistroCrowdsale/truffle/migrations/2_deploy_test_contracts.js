68c0dc87c51fb38a421dcf38b97d816aa8cd980b
H <hackdom@majoolr.io>
2017-12-08 04:54:41 -0600

    temp-commit

--- CrowdsaleLib/EvenDistroCrowdsale/truffle/migrations/2_deploy_test_contracts.js
+++ CrowdsaleLib/EvenDistroCrowdsale/truffle/migrations/2_deploy_test_contracts.js
@@ -3,17 +3,9 @@ var Array256Lib = artifacts.require("./Array256Lib.sol");
 var TokenLib = artifacts.require("./TokenLib.sol");
 var CrowdsaleLib = artifacts.require("./CrowdsaleLib.sol");
 var EvenDistroCrowdsaleLib = artifacts.require("./EvenDistroCrowdsaleLib.sol");
-var EvenDistroCrowdsaleTestContract = artifacts.require("./EvenDistroCrowdsaleTestContract.sol");
 
-//testrpc contracts
-var TestCrowdsaleLib = artifacts.require("./TestCrowdsaleLib.sol");
-var TestEvenDistroCrowdsaleLib = artifacts.require("./TestEvenDistroCrowdsaleLib.sol");
-
-var CrowdsaleToken = artifacts.require("./CrowdsaleToken.sol");
-var TimeEvenDistroCrowdsaleTestContract = artifacts.require("./TimeEvenDistroCrowdsaleTestContract.sol");
-
-var CrowdsaleToken2 = artifacts.require("./CrowdsaleToken2.sol");
-var TimeEvenDistroCrowdsaleTestContract2 = artifacts.require("./TimeEvenDistroCrowdsaleTestContract2.sol");
+var CrowdsaleTestTokenEteenD = artifacts.require("./CrowdsaleTestTokenEteenD");
+var EvenDistroTestEteenD = artifacts.require("./EvenDistroTestEteenD.sol");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(BasicMathLib,{overwrite: false});
@@ -29,30 +21,42 @@ module.exports = function(deployer, network, accounts) {
   deployer.deploy(EvenDistroCrowdsaleLib, {overwrite:false});
 
   if(network === "development" || network === "coverage"){
-    deployer.link(BasicMathLib,TestCrowdsaleLib);
-    deployer.link(TokenLib,TestCrowdsaleLib);
-    deployer.deploy(TestCrowdsaleLib);
-    deployer.link(BasicMathLib,TestEvenDistroCrowdsaleLib);
-    deployer.link(TokenLib,TestEvenDistroCrowdsaleLib);
-    deployer.link(TestCrowdsaleLib,TestEvenDistroCrowdsaleLib);
-    deployer.deploy(TestEvenDistroCrowdsaleLib);
-    deployer.link(TokenLib,CrowdsaleToken);
-    deployer.link(TestCrowdsaleLib,TimeEvenDistroCrowdsaleTestContract);
-    deployer.link(TestEvenDistroCrowdsaleLib, TimeEvenDistroCrowdsaleTestContract);
-    deployer.link(TokenLib,CrowdsaleToken2);
-    deployer.link(TestCrowdsaleLib,TimeEvenDistroCrowdsaleTestContract2);
-    deployer.link(TestEvenDistroCrowdsaleLib, TimeEvenDistroCrowdsaleTestContract2);
-    deployer.deploy(CrowdsaleToken, accounts[5], "Tester Token", "TST", 18, 20000000000000000000000000, false, {from:accounts[5]}).then(function() {
-      // configured to set the token price to $1.41, with a periodic increase in the address cap by 250%
-      var purchaseData =[105,141,100,
-                         110,141,250,
-                         115,141,625];
-      return deployer.deploy(TimeEvenDistroCrowdsaleTestContract, accounts[5], 100, purchaseData, 29000, 1700000000, 125, 50, 50000, false, CrowdsaleToken.address,{from:accounts[5]});
-    });
-    deployer.deploy(CrowdsaleToken2, accounts[5], "Tester Token", "TST", 0, 500000000, false, {from:accounts[5]}).then(function() {
-      var purchaseData2 =[105,50,50000,
-                         135,75,50000];
-      return deployer.deploy(TimeEvenDistroCrowdsaleTestContract2, accounts[5], 100, purchaseData2, 29000, 40000000000, 165, 100, 50000, true, CrowdsaleToken2.address,{from:accounts[5]});
+    deployer.link(TokenLib,CrowdsaleTestTokenEteenD);
+    deployer.link(CrowdsaleLib,EvenDistroTestEteenD);
+    deployer.link(EvenDistroCrowdsaleLib, EvenDistroTestEteenD);
+
+    // startTime 3 days + 1 hour in the future
+    var startTime = (Math.floor((new Date().valueOf()/1000))) + 262800;
+    // first price step in 7 days
+    var stepOne = startTime + 604800;
+    // second price step in 14 days
+    var stepTwo = stepOne + 604800;
+    // endTime in 30 days
+    var endTime = startTime + 2592000;
+    deployer.deploy(CrowdsaleTestTokenEteenD,
+                    accounts[0],
+                    "Eighteen Decimals",
+                    "ETEEN",
+                    18,
+                    50000000000000000000000000,
+                    false,
+                    {from:accounts[5]})
+    .then(function() {
+      console.log(CrowdsaleTestTokenEteenD.address);
+      var purchaseData =[startTime,50,100,
+                         stepOne,75,100,
+                         stepTwo,100,0];
+ 	    return deployer.deploy(EvenDistroTestEteenD,
+                             accounts[5],
+                             purchaseData,
+                             45000,
+                             9000000,
+                             endTime,
+                             100,
+                             10000000000000000000000,
+                             false,
+                             CrowdsaleTestTokenEteenD.address,
+                             {from:accounts[5]})
     });
   }
 };
