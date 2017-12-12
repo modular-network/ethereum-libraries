3a60f0090d16760ced8eace480c1b322d6413949
H <hackdom@majoolr.io>
2017-12-12 01:11:23 -0600

    fix-test-port

--- WalletLib/truffle/truffle.js
+++ WalletLib/truffle/truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     coverage: {
       host: "localhost",
-      port: 8545,
+      port: 8555,
       from: "0x40333d950b4c682e8aad143c216af52877d828bf",
       network_id: "*",
       gas: 0xfffffffffff,
@@ -10,7 +10,7 @@ module.exports = {
     },
     development: {
       host: "localhost",
-      port: 8555,
+      port: 8545,
       gas: 270000000000000,
       from: "0x40333d950b4c682e8aad143c216af52877d828bf",
       network_id: "*"
